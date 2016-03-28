8965eed0abbfdea4e6c6a60b43ccfd33afbd2ac3
Poventrant <296306654@qq.com>
2016-03-28 22:06:20 +0800

    Update ArPixivUtil.java

--- app/utils/ArPixivUtil.java
+++ app/utils/ArPixivUtil.java
@@ -1,15 +1,3 @@
-/*
-* *
-*  *
-*  * 功能描述：
-*  * <p> 版权所有：优视科技
-*  * <p> 未经本公司许可，不得以任何方式复制或使用本程序任何部分 <p>
-*  *
-*  * @author <a href="wb-pwq174842@alibaba-inc.com">庞文全</a>
-*  * @version 1.0.1
-*  * create on: 2016年
-*  *
-*/
 
 package utils;
 
